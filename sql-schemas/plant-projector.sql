654feebd447dbcac59bff7db29d0c12227b15536
Xivius <nsong4@hawk.iit.edu>
2021-02-14 13:40:52 -0600

    Update schema

--- sql-schemas/plant-projector.sql
+++ sql-schemas/plant-projector.sql
@@ -1,11 +1,9 @@
 CREATE TABLE `plant_data` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) NOT NULL,
-  `day_temp_upper` int DEFAULT NULL,
-  `day_temp_lower` int DEFAULT NULL,
-  `night_temp_upper` int DEFAULT NULL,
-  `night_temp_lower` int DEFAULT NULL,
-  `absolute_min_temp` int DEFAULT NULL,
-  `absolute_max_temp` int DEFAULT NULL,
+  `temp_upper` int DEFAULT NULL,
+  `temp_lower` int DEFAULT NULL,
+  `abs_temp_upper` int DEFAULT NULL,
+  `abs_temp_lower` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
